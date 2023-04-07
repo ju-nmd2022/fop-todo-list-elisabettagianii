b1c4228342b3af7b66452c8770aac8ac08741aa3
elisabettagianii <elisabettagiani@yahoo.com>
2023-04-07 17:25:13 +0200

    progress 2\3
    I made the function that can make the user remove the item, via an X button. Now I'll add the mark-as-complete function and then make all look nice

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
+// I have consulted the tutorial website https://www.w3schools.com/howto/howto_js_todolist.asp
 
 
-// Create a new list item when clicking on the "Add" button
 function newElement() {
-  let li = document.createElement("li");
-  // I create a list where to store the new task that i'll write
-  let newTask = document.getElementById("textNewTask").value;
-  // i create a new text node in the DOM
-  let t = document.createTextNode(newTask);
-  // I let the list be a child of the parent element "t"
-  li.appendChild(t);
-  if (newTask === "") {
+  // I create a list where to put the new task that i'll write in the input field 
+  let itemOfTheList = document.createElement("li");
+  let textNewTask = document.getElementById("textNewTask").value;
+  let taskText = document.createTextNode(textNewTask);
+  itemOfTheList.appendChild(taskText);
+  if (textNewTask === "") {
     alert("Empty");
   } else {
     // if it's not empty i add it to the list, and then i set back the value of the input text to be empty again
-    document.getElementById("taskList").appendChild(li);
+    document.getElementById("taskList").appendChild(itemOfTheList);
   }
   document.getElementById("textNewTask").value = "";
+
+  // i create a X button to add to all new added task, so I can delete them
+  let deleteButton = document.createElement("button");
+  deleteButton.innerText = "X";
+  deleteButton.idName = "buttonX";
+  itemOfTheList.appendChild(deleteButton);
+
+  // when clicked, the X button removes the task to do.
+  deleteButton.addEventListener("click", function () {
+    itemOfTheList.remove();
+  });
 }
+
+
+
