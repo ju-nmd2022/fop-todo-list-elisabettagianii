9e0289b84276586fa0b6c11343ce675a11e7c036
elisabettagianii <elisabettagiani@yahoo.com>
2023-04-07 16:26:02 +0200

    adding tasks
    I made the feature of adding tasks, now i need to work on marking them as done and deleting them;

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+
+
+// Create a new list item when clicking on the "Add" button
 function newElement() {
-    
+  let li = document.createElement("li");
+  // I create a list where to store the new task that i'll write
+  let newTask = document.getElementById("textNewTask").value;
+  // i create a new text node in the DOM
+  let t = document.createTextNode(newTask);
+  // I let the list be a child of the parent element "t"
+  li.appendChild(t);
+  if (newTask === "") {
+    alert("Empty");
+  } else {
+    // if it's not empty i add it to the list, and then i set back the value of the input text to be empty again
+    document.getElementById("taskList").appendChild(li);
   }
+  document.getElementById("textNewTask").value = "";
+}
